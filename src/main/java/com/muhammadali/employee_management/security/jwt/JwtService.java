765c352e97f19c279176a3e2702891cc5ae072e6
Muhammadali <qoxxoraliyev.muhammadali2006@gmail.com>
2026-03-12 16:20:40 +0500

    initial commit

--- src/main/java/com/muhammadali/employee_management/security/jwt/JwtService.java
+++ src/main/java/com/muhammadali/employee_management/security/jwt/JwtService.java
@@ -39,7 +39,7 @@ public class JwtService {
                     .setClaims(claims)
                     .setSubject(email)
                     .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 min
+                    .setExpiration(new Date(System.currentTimeMillis() +1000 * 60 * 60 * 24)) // 24 hour
                     .signWith(getSignKey(), SignatureAlgorithm.HS256)
                     .compact();
         } catch (Exception e) {
