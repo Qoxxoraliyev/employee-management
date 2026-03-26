e4fa7038b7a8f6380005d7aa4606fbd24fdbd121
Muhammadali <qoxxoraliyev.muhammadali2006@gmail.com>
2026-03-26 15:59:15 +0500

    initial commit

--- src/main/java/com/muhammadali/employee_management/entity/Users.java
+++ src/main/java/com/muhammadali/employee_management/entity/Users.java
@@ -27,7 +27,7 @@ public class Users implements Serializable {
 
     @NotBlank(message = "Username cannot be blank")
     @Size(min = 3,max = 50,message = "Username must be between 3 and 50 characters")
-    @Column(name = "email",nullable = false,unique = true,length = 50)
+    @Column(name = "username",nullable = false,unique = true,length = 50)
     private String username;
 
     @NotBlank(message = "Password cannot be blank")
